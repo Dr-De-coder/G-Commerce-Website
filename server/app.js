0658f5530b30c167c3dec1c7ebf84b4a894a20a3
Sachin Kumar Sah <sachinhangupcrew@gmail.com>
2026-05-09 20:51:18 +0530

    Fixed server folder tracking

---
+++ server/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import { config } from "dotenv";
+import cors from "cors";
+import { createTables } from "./utils/createTables.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+
+
+const app = express();
+
+config({ path: "./config/config.env"});
+
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(
+    fileUpload({
+        tempFileDir: "./upload",
+        useTempFiles: true,
+    })
+)
+
+createTables()
+
+app.use(errorMiddleware)
+
+export default app;
